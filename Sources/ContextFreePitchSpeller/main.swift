439635bae3547bc60c16c0d9be699fec0476b396
Ben Wetherfield <bwetherfield@gmail.com>
2019-06-27 20:21:54 -0700

    Add JSON decoding functionality

--- Sources/ContextFreePitchSpeller/main.swift
+++ Sources/ContextFreePitchSpeller/main.swift
@@ -1,1 +1,15 @@
+import Foundation
+import PitchSpeller
 
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
+
+let filepath = getDocumentsDirectory().appendingPathComponent("pitched_weights.json")
+let data = try Data(contentsOf: filepath)
+
+let decoder = JSONDecoder()
+let lookupTable = try decoder.decode([PitchSpeller.SpellingInverter.PitchedEdge: Double].self, from: data)
+
+print(lookupTable)
