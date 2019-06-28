381fb943fd73b47344527778a864b202a2956854
Ben Wetherfield <bwetherfield@gmail.com>
2019-06-27 20:31:36 -0700

    Improve error syntax

--- Sources/ContextFreePitchSpeller/main.swift
+++ Sources/ContextFreePitchSpeller/main.swift
@@ -7,9 +7,15 @@ func getDocumentsDirectory() -> URL {
 }
 
 let filepath = getDocumentsDirectory().appendingPathComponent("pitched_weights.json")
-let data = try Data(contentsOf: filepath)
 
 let decoder = JSONDecoder()
-let lookupTable = try decoder.decode([PitchSpeller.SpellingInverter.PitchedEdge: Double].self, from: data)
 
-print(lookupTable)
+do {
+    let data = try Data(contentsOf: filepath)
+    let lookupTable = try decoder.decode([PitchSpeller.SpellingInverter.PitchedEdge: Double].self, from: data)
+    print(lookupTable)
+} catch let error {
+    print(error.localizedDescription)
+}
+
+
